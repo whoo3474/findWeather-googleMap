236a8db175dc3a6b967645593faedff83da395ae
JeongMin Woo <minhan_a@naver.com>
2018-10-23 08:29:53 +0900

    serch_bar & input-group form create

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 
+import SerchBar from '../containers/serch_bar'
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <SerchBar />
+      </div>
     );
   }
 }
