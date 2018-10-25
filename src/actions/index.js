75e9f54ae6da8e00aa29958bd61ac98c44fb1c55
JeongMin Woo <minhan_a@naver.com>
2018-10-25 12:07:51 +0900

    mapDispatchToProps fetchWeather & connect

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = '963f998eb4eac7b3711a168b6256eafa';
+const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`
 
 export const FETCH_WEATHER='FETCH_WEATHER';
 
-export function fetchWearher(){
+export function fetchWearher(city){
+  const url =`${ROOT_URL}&q=${city},kr`
+  const request = axios.get(url);
+
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   }
 }
--- src/containers/serch_bar.js
+++ src/containers/serch_bar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index'; 
 
-export default class SearchBar extends Component{
+class SearchBar extends Component{
 
   constructor(props){
     super(props);
@@ -36,3 +39,9 @@ export default class SearchBar extends Component{
     )
   }
 }
+
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({fetchWeather},dispatch);
+}
+
+export default connect(null,mapDispatchToProps);
